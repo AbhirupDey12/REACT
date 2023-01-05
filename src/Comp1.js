1b342cd6cefce091086b52364bb7cc559246b053
abhirupdey15 <abhirupd490@gmail.com>
2023-01-05 20:13:29 +0530

    little change

--- src/Comp1.js
+++ src/Comp1.js
@@ -8,16 +8,16 @@ export function CompText(props) {
 
 export default function Comp1() {
     
-    let [count, setText] = React.useState(0);
+    let [count, setCount] = React.useState(0);
 
     // Here , the first argument that we have recived in the array is the state and the second argument is the setter which we use to re render the updated value in our UI
 
     function increaseCount() {
-        setText(++count);
+        setCount(++count);
     }
 
     function decreaseCount() {
-        setText(--count);
+        setCount(--count);
     }
 
     return (
